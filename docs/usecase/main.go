d7b680ee7033a44a7cafe0e186a1d51086e4510b
julio <julio.grillo98@gmail.com>
2020-10-26 01:46:00 -0700

    docs: use case links

--- docs/usecase/main.go
+++ docs/usecase/main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"regexp"
+	"sort"
 	"strconv"
 	"strings"
 
@@ -49,9 +51,15 @@ func main() {
 			ListOptions: github.ListOptions{PerPage: 100},
 		})
 
+	sort.Slice(issues, func(i, j int) bool {
+		return *issues[i].Number < *issues[j].Number
+	})
+
 	if err != nil {
 		panic(err)
 	}
+
+	anchors := table(issues)
 
 	for _, v := range issues {
 		f := os.Stdout
@@ -65,14 +73,14 @@ func main() {
 			}
 		}
 
-		title := strings.Replace(*v.Title, "use case:", "", -1)
-		title = strings.Title(strings.TrimSpace(title))
 
-		fmt.Fprintf(f, "# `#%d` %s\n\n",
-			*v.Number,
-			title)
+		fmt.Fprintf(f,
+			useCaseTitle(v)+
+			"\n\n")
 
-		fmt.Fprintln(f, *v.Body)
+		body := githubIssueLinks(anchors, *v.Body)
+
+		fmt.Fprintln(f, body)
 		fmt.Fprint(f,
 			"\n\n"+
 			`<hr>`+
@@ -85,4 +93,93 @@ func main() {
 			f.Close()
 		}
 	}
+}
+
+
+var githubIssueLinksRegex = regexp.MustCompile("#[0-9]+")
+
+func githubIssueLinks(anchors map[int]string, body string) string {
+	body = strings.Replace(body, "\x0d", "", -1)
+	return githubIssueLinksRegex.ReplaceAllStringFunc(body,
+		func(in string) string {
+			number, _ := strconv.Atoi(in[1:])
+			if number == 0 {
+				return in
+			}
+
+			return fmt.Sprintf(`<a href="#%s">%s</a>`,
+				anchors[number],
+				in)
+		})
+}
+
+func useCaseName(v *github.Issue) string {
+	title := strings.Replace(*v.Title, "use case:", "", -1)
+	title = strings.Title(strings.TrimSpace(title))
+
+	return fmt.Sprintf(`<code>#%d</code> %s`,
+		*v.Number,
+		title)
+}
+
+func useCaseAnchor(v *github.Issue) string {
+	name := strings.Replace(*v.Title, "use case:", "", -1)
+	name = strings.Title(strings.TrimSpace(name))
+	name = strings.ToLower(name)
+	return strings.Replace(name, " ", "-", -1)
+}
+
+func useCaseTitle(v *github.Issue) string {
+	return fmt.Sprintf(`<h1><a name="%s">%s</a></h1>`,
+		useCaseAnchor(v),
+		useCaseName(v))
+}
+
+func table(issues []*github.Issue) map[int]string {
+	anchors := make(map[int]string)
+
+	f := os.Stdout
+	if len(*dir) != 0 {
+		fname := path.Join(*dir, "toc.md")
+		var err error
+		f, err = os.OpenFile(fname,
+			os.O_CREATE|os.O_WRONLY|os.O_TRUNC,
+			0644)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	fmt.Fprint(f,
+		`<h1>Table of Contents</h1>`+
+		"\n"+
+		`<ul>`+
+		"\n\n")
+
+	for _, v := range issues {
+		anchors[*v.Number] = useCaseAnchor(v)
+
+		fmt.Fprintf(f,
+			`<li><a href="#%s">%s</a></li>`+"\n",
+			useCaseAnchor(v),
+			useCaseName(v))
+	}
+
+	fmt.Fprint(f,
+		`</ul>`+
+		"\n\n")
+
+	fmt.Fprint(f,
+		"\n\n"+
+		`<hr>`+
+		"\n\n"+
+		`<div style="page-break-after: always;"></div>`+
+		"\n\n")
+
+	if len(*dir) != 0 {
+		f.Sync()
+		f.Close()
+	}
+
+	return anchors
 }
