f9af1b10391b1d2f0cfbca078b56b88788683754
Matthew Panelo <mcpanelo@ucsd.edu>
2020-12-08 22:39:54 -0800

    Scraping should now collect all information from Facebook necessary to create an event model. On inputting an organization's event page, each upcoming event will be scraped.

--- orgproxy/main.go
+++ orgproxy/main.go
@@ -23,27 +23,94 @@ func ChromedpDebugOpts() []func(*chromedp.ExecAllocator) {
 }
 
 func GetEvents(url string) ([]models.Event, error) {
-	// create context
-	allocCtx, _ := chromedp.NewExecAllocator(
-		context.Background(),
-		ChromedpDebugOpts()...)
+	// return info
+        const eventsRoot = "https://www.facebook.com/events/"
+        const descTrimL = "/events/"
+        const descTrimR = "?acontext=%7B%22ref%22%3A51%2C%22source%22%3A5%2C%22action_history%22%3A[%7B%22surface%22%3A%22page%22%2C%22mechanism%22%3A%22main_list%22%2C%22extra_data%22%3A%22%5C%22[]%5C%22%22%7D]%2C%22has_source%22%3Atrue%7D"
+        const eventNodesPath = `(//*[@id="upcoming_events_card"])//span[@class=" _50f7"]`
 
-	ctx, cancel := chromedp.NewContext(allocCtx)
-	defer cancel()
+        var org string
+        var name []string
+        var timeInfo []string
+        var description []string
 
-	// run task list
-	var res string
-	err := chromedp.Run(ctx,
-		chromedp.Navigate(`https://www.facebook.com/pg/csesucsd/events/`),
-	)
+        var eventNodes []*cdp.Node
+        var linkNodes []*cdp.Node
+        var tags [][]string
 
-	if err != nil {
-		return nil, err
-	}
+        var newURL string
+        var attrib map[string]string
 
-	err = chromedp.Run(ctx,
-		chromedp.Text(`//*[@class="_2l3f _2pic"]`, &res),
-	)
+        // create context
+        allocCtx, _ := chromedp.NewExecAllocator(
+                context.Background(),
+                ChromedpDebugOpts()...)
+
+        ctx, cancel := chromedp.NewContext(allocCtx)
+        defer cancel()
+
+        //Navigate to the Org page containing all events
+        err := chromedp.Run(ctx,
+                chromedp.Navigate( url ),
+        )
+
+        //Error trying to navigate to specified url
+        if err != nil {
+                return err, org, "", "", ""
+        }
+
+	//Get org name and number of events
+        err = chromedp.Run(ctx,
+                chromedp.Nodes( eventNodesPath, &eventNodes),
+                chromedp.Nodes( `//*[@class="_4dmk"]/a`, &linkNodes),
+                chromedp.Text(`//*[@class="_64-f"]`, &org),
+        )
+
+        //Adjust the array sizes to the number of upcoming events
+        name = make([]string, len( eventNodes ))
+        timeInfo = make([]string, len( eventNodes ))
+        description = make([]string, len( eventNodes ))
+        tags = make([][]string, len( eventNodes ))
+
+        //Get details for each event
+        for i := 0; i < len( eventNodes ); i++ {
+        
+                //Get event name
+                err = chromedp.Run(ctx,
+                        chromedp.Text( eventNodes[i].FullXPath() , &name[i] ),
+                )
+
+                //Navigate to the specific event Facebook page
+                err = chromedp.Run(ctx,
+                        chromedp.Attributes( linkNodes[i].FullXPath() , &attrib ),
+                )
+                newURL =  attrib["href"]
+                newURL = strings.TrimLeft( newURL, descTrimL )
+                newURL = strings.TrimRight( newURL, descTrimR )
+                newURL = eventsRoot + newURL
+
+                var tagList []*cdp.Node
+
+                //Get event description and time details        
+                err = chromedp.Run(ctx,
+                        chromedp.Navigate( newURL ),
+                        chromedp.Text( `//*[@class="_63ew"]/span`, &description[i] ),
+                        chromedp.AttributeValue( `//*[@class="_2ycp _5xhk"]`, "content", &timeInfo[i], nil ),
+                        chromedp.Nodes(`//*[@class="_63ep _63eq"]/a`, &tagList),
+                )
+		
+		//Get the tags for this event
+                tags[i] = make([]string, len( tagList ))
+                for j := 0; j < len( tagList ); j++ {
+                        err = chromedp.Run(ctx,
+                                chromedp.Text( tagList[j].FullXPath(), &tags[i][j] ),
+                        )
+                }
+                err = chromedp.Run(ctx,
+                        chromedp.Navigate( url ),
+                )
+
+        }
 
 	return nil, nil
 }
